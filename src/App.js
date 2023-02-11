7ae305e486f0c05eb0d9db3b89fc0fb4a28d3170
Seogun95 <ksj083895@gmail.com>
2023-02-12 04:26:55 +0900

    feat: payload를 이용하여 counter 앱 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { plusN } from './redux/modules/counter';
+import { minusN } from './redux/modules/counter';
 
 const App = () => {
   //store에 접근
@@ -12,22 +14,26 @@ const App = () => {
 
   //카운트 증가 함수
   const countUpHandler = () => {
-    dispatch({
-      type: 'PLUS_ONE',
-    });
+    dispatch(plusN(number));
   };
 
   //카운트 감소 함수
   const countDownHandler = () => {
-    dispatch({
-      type: 'MINUS_ONE',
-    });
+    dispatch(minusN(number));
+  };
+
+  //input state
+  const [number, setNumber] = useState(0);
+
+  const countInputHandler = (e) => {
+    setNumber(e.target.value);
   };
 
   return (
     <>
       <div>
-        <h1>현재 카운트: {data.number}</h1>
+        <h1>서근 카운트: {data.number}</h1>
+        <input type="number" value={number} onChange={countInputHandler} />
         <button onClick={countUpHandler}>+</button>
         <button onClick={countDownHandler}>-</button>
       </div>
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,14 +1,36 @@
+//Action value
+const PLUS_N = 'PLUS_N';
+const MINUS_N = 'MINUS_N';
+
+//Action creator: action value를 return하는 함수
+
+export const plusN = (payload) => {
+  return {
+    type: PLUS_N,
+    payload: Number(payload), // 문자열로 넘어오는것을 Number로 바꿔줌
+  };
+};
+
+export const minusN = (payload) => {
+  return {
+    type: MINUS_N,
+    payload: Number(payload), // 문자열로 넘어오는것을 Number로 바꿔줌
+  };
+};
+
 //초기 상태 값 (state)
 const initialState = {
   number: 0,
 };
 
+//action 객체 라는것은 action type을 payload 만큼 처리하는것이다.
+//ex: playload가 3이다. 3만큼을 증가 해라
 const counter = (state = initialState, action) => {
   switch (action.type) {
-    case 'PLUS_ONE':
-      return { number: state.number + 1 };
-    case 'MINUS_ONE':
-      return { number: state.number - 1 };
+    case PLUS_N:
+      return { number: state.number + action.payload };
+    case MINUS_N:
+      return { number: state.number - action.payload };
     default:
       return state;
   }
